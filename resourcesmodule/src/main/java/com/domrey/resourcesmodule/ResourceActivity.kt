e854f08567220f95d0b7dc24a66bbb7d1df889b0
Khunchheang <khunchheangleang100@gmail.com>
2020-10-17 21:12:17 +0700

    Make resource module as application

---
+++ resourcesmodule/src/main/java/com/domrey/resourcesmodule/ResourceActivity.kt
@@ -0,0 +1,17 @@
+package com.domrey.resourcesmodule
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Created by : khunchheang
+ * Date  : 10/17/20
+ * Time  : 9:09 PM
+ */
+class ResourceActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.loading_state)
+    }
+}
